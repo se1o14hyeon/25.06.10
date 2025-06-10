ee83babc261a8bf22d2a48c35dcc9bc78ee590ae
se1o14hyeon <c1371098@naver.com>
2025-06-10 14:38:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("나의첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="홍길동":
+    st.success("반갑습니다, 홍길동님!")
+  else:
+    st.warming("누구세요?")
